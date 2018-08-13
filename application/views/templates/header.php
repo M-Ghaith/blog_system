783237adaad4998e1d79d5c7130001657fdbfb53
MG Alseirawan <ghaithprincegh@gmail.com>
2018-08-13 22:36:00 +0200

    Create the route, initializing the app

---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Blog system</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
+    </head>
+    <nav class="navbar navbar-inverse">
+            <div class="container" >
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">blog</a>
+                </div>
+                <div id="navbar" >
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?= base_url();?>">Home</a></li>
+                        <li><a href="<?= base_url();?>about">About</a></li>
+                        <li><a href="<?= base_url();?>posts">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    <body>
+        <div class="container" >

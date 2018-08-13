58b296565938e55f55f70a53fac2e9558191a94e
MG Alseirawan <ghaithprincegh@gmail.com>
2018-08-13 23:12:53 +0200

    Adding database and getting the posts from posts table, display the posts in the blog main page

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,1 +1,6 @@
-this is posts
+<h1>Posts</h1>
+<?php foreach($posts as $post) : ?>
+    <h3><?= $post['title']; ?></h3>
+    <small>Posted on: <?= $post['created_at'];?></small>
+    <p><?= $post['body'];?></p>
+    <?php endforeach?>
